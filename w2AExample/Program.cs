c8420156554aca6b663e3dbd00ab443b544a5761
genesis <pearcshaw@sheridancollege.ca>
2025-05-14 13:30:14 -0400

    This is my first update (testing git from Rider)

--- w2AExample/Program.cs
+++ w2AExample/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// C# Fundementals
+//By Shawn Pearce
+Console.WriteLine("example 1");
+int a=5;
+double b=a;
+double c=5.5;
+int d=(int)c; //Explicit conversion
+Console.WriteLine($"a:{a} b:{b} c:{c} d:{d}");
+var e =5.5;
+//e="4"
